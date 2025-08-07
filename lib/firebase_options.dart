6cd6c9becbaf03b6a14ae12d6cb58eab9b018b68
mislamdev <mislamdev@mitech.stream>
2025-08-07 11:58:47 +0600

    v1.0.0
    Stable.

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,16 +2,6 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
-/// Default [FirebaseOptions] for use with your Firebase apps.
-///
-/// Example:
-/// ```dart
-/// import 'firebase_options.dart';
-/// // ...
-/// await Firebase.initializeApp(
-///   options: DefaultFirebaseOptions.currentPlatform,
-/// );
-/// ```
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
@@ -48,11 +38,11 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'YOUR_API_KEY',
-    appId: 'YOUR_APP_ID',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_STORAGE_BUCKET',
+    apiKey: 'AIzaSyAzcMl7YzEbeuTmRHcYOV-Hj0IFfYjO9nY',
+    appId: '1:472609125098:android:456335306945849f919d93',
+    messagingSenderId: '472609125098',
+    projectId: 'mtcare-1dda4',
+    storageBucket: 'mtcare-1dda4.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
